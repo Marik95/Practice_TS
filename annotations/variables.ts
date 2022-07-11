a3afa2cbbc18b641fb8381815dbec573ab0417f7
Alexey <alexmarchenko70@gmail.com>
2022-07-11 16:38:20 +0300

    annotations for variables in TypeScript

---
+++ annotations/variables.ts
@@ -0,0 +1,11 @@
+//number is annotation
+let speed:number;
+let color:string = "red";
+let nitro:boolean= true;
+speed = 250; //true
+//speed = "fast"; false
+
+
+//built-in objects
+
+let now:Date = new Date();
