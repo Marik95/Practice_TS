58924b27d7416c78e05e627e85f23e1c3e7e11bd
Alexey <alexmarchenko70@gmail.com>
2022-07-14 16:56:57 +0300

    objects annotations lesson

---
+++ annotations/objects.ts
@@ -0,0 +1,19 @@
+//array annotation
+let colors: string[] = ['red', 'yellow', 'grey'];
+let numbers: number[] = [1, 2, 3];
+
+
+//classes annotation
+
+class Car{
+
+}
+let car: Car = new Car();
+
+
+//object literal
+
+let point: {x: number; y: number} = {
+    x: 20,
+    y: 10
+}
