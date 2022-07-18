18ceb57b46f6991ce3db156179141626387be6b2
Alexey <alexmarchenko70@gmail.com>
2022-07-18 15:23:46 +0300

    Destructurization in objects TS

--- annotations/objects.ts
+++ annotations/objects.ts
@@ -17,3 +17,23 @@ let point: {x: number; y: number} = {
     x: 20,
     y: 10
 }
+
+
+//Destructurization in objects TS
+
+const profile = {
+    name: 'Jack',
+    age: 25,
+    coordinates: {
+        lat: 1,
+        lng: 20
+    },
+    setAge(age: number): void {
+        this.age = age;
+    }
+}
+
+//ES2015
+const {age}: {age:number} = profile;
+
+const {coordinates: {lat, lng}}: {coordinates: {lat: number; lng: number}} = profile;
