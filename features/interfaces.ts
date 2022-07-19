a3388bb9cab550965a660afe0351d8a44c416df3
Marik95 <alexmarchenko70@gmail.com>
2022-07-19 15:58:42 +0300

    Refactoring the code, to show more features with interfaces

--- features/interfaces.ts
+++ features/interfaces.ts
@@ -1,5 +1,4 @@
-
-// Interface initializtion, creating a new type
+// #1 Interface initializtion, creating a new type
 interface Vehicle {
     name: string;
     year: Date;
@@ -24,3 +23,37 @@ const printVehicle = (vehicle: Vehicle): void => {
 
 //run function with object in entering data
 printVehicle(oldCivic);
+
+
+
+
+// #2 Refactoring
+interface Reportable {
+    summary(): string;
+}
+
+const oldCivic2 = {
+    name: 'Civic',
+    year: new Date(),
+    broken: true,
+    summary(): string {
+        return `Name: ${this.name}, Year: ${this.year}, Broken: ${this.broken}`;
+    }
+}
+
+const cola2 = {
+    color: 'brown',
+    carbonated: true,
+    sugar: 40,
+    summary(): string{
+        return `Cola has ${this.sugar} grams of sugar`;
+    },
+}
+
+
+const printSummary = (item: Reportable): void => {
+    console.log(item.summary());
+}
+
+printSummary(oldCivic2);
+printSummary(cola2);
