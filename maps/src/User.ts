50e2313c4c4cc827bd7c54ec962eff57f59feb46
Marik95 <alexmarchenko70@gmail.com>
2022-07-21 19:45:59 +0300

    create the constructor of User class

--- maps/src/User.ts
+++ maps/src/User.ts
@@ -6,4 +6,12 @@ class User {
         lat: number;
         lng: number;
     };
+    constructor() {
+        this.name = faker.name.firstName();
+        this.location = {
+            lat: parseFloat(faker.address.latitude()),
+            lng: parseFloat(faker.address.longitude())
+        }
+        
+    }
 }
