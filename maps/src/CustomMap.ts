484f6583e2bb3b43a82619ad7a12ea28ab87b6a4
Marik95 <alexmarchenko70@gmail.com>
2022-08-24 11:11:37 +0300

    add class CustomMap with private feature googleMap, modify CustomMap for getting dynamic id

---
+++ maps/src/CustomMap.ts
@@ -0,0 +1,15 @@
+import {} from "google-maps";
+export class CustomMap{
+    private googleMap: google.maps.Map;
+
+    constructor(mapDivId: string){
+        this.googleMap = new google.maps.Map(document.getElementById(mapDivId) as HTMLElement, {
+            zoom: 1,
+            center: {
+                lat: 0,
+                lng: 0
+            }
+        
+        });
+    }
+}
--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,14 +1,8 @@
 // import { User } from "./User";
 // import { Company } from "./Company";
-import {} from "google-maps";
+import { CustomMap } from "./CustomMap";
 
 
-let map_div = document.getElementById('map') as HTMLElement;
-new google.maps.Map(map_div, {
-    zoom: 1,
-    center: {
-        lat: 0,
-        lng: 0
-    }
+new CustomMap('map');
 
-});
+
