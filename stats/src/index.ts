8c0e5d00493bb6b349c4db902408cbd0d195d016
Marik95 <alexmarchenko70@gmail.com>
2022-09-06 16:14:04 +0300

    second refactoring of CsvFileReader by using interface DataReader

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,14 +1,21 @@
 import { MatchReader } from "./MatchReader";
 import { MatchResult } from "./MatchResult";
+import { CsvFileReader } from "./CsvFileReader";
 
-const reader = new MatchReader('football.csv');
-reader.read();
+//Create an object that implement the DataReader interface
+const csvFileReader = new CsvFileReader('football.csv');
 
+
+// Create an object of MatchReader and pass DataReader implementation
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
+
+// matchReader.matches
 
 
 let manUnitedWins = 0;
 
-for( let match of reader.data){
+for( let match of matchReader.matches){
     if(match[1] === 'Man United' && match[5] === MatchResult.HomeWin){
         manUnitedWins++;
     } else if(match[2] === 'Man United' && match[5] === MatchResult.AwayWin){
