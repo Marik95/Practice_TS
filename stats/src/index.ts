f65e75c22ee1798589e438e3aa88a140a98e0b05
Marik95 <alexmarchenko70@gmail.com>
2022-09-06 17:45:33 +0300

    Add static methods to classes Summary and MatchReader

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,20 +1,22 @@
 import { MatchReader } from "./MatchReader";
-import { CsvFileReader } from "./CsvFileReader";
-import { WinsAnalysis } from "./Analyzers/WinsAnalysis";
-import { ConsoleReport } from "./reportTargets/ConsoleRecoprt";
+// import { CsvFileReader } from "./CsvFileReader";
+// import { WinsAnalysis } from "./Analyzers/WinsAnalysis";
+// import { ConsoleReport } from "./reportTargets/ConsoleRecoprt";
 import { Summary } from "./Summary";
-import { HtmlReport } from "./reportTargets/HtmlReport";
+// import { HtmlReport } from "./reportTargets/HtmlReport";
 
 //Create an object that implement the DataReader interface
-const csvFileReader = new CsvFileReader('football.csv');
+// const csvFileReader = new CsvFileReader('football.csv');
 
 
 // Create an object of MatchReader and pass DataReader implementation
-const matchReader = new MatchReader(csvFileReader);
+// const matchReader = new MatchReader(csvFileReader);
+const matchReader = MatchReader.fromCsv('football.csv');
 matchReader.load();
 
 
 // const summary = new Summary(new WinsAnalysis('Man United'), new ConsoleReport());
-const summary = new Summary(new WinsAnalysis('Man United'), new HtmlReport());
+// const summary = new Summary(new WinsAnalysis('Man United'), new HtmlReport());
+const summary = Summary.winsAnalysisWithHtmlReport('Everton');
 summary.buildAndPrintReport(matchReader.matches);
 
