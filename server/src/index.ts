7e36f90c3fdde628eac2fbd21a3da98ba1133349
Marik95 <alexmarchenko70@gmail.com>
2022-09-06 22:57:50 +0300

    Cretae first route in loginRoutes, start working with routes

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,19 @@
 import express, {Request, Response} from 'express';
+import { router } from './routes/loginRoutes';
 
 const app = express();
 
-app.get('/', (req: Request, res: Response) => {
-    res.send(
-        `
-            <div>
-                <h1>Hey there!</h1>
-            </div>
-        `
-    )
-});
+// app.get('/', (req: Request, res: Response) => {
+//     res.send(
+//         `
+//             <div>
+//                 <h1>Hey there!</h1>
+//             </div>
+//         `
+//     )
+// });
+
+app.use(router);
 
 app.listen(3001, () => {
     console.log('Listening on port 3001...')
