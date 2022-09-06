f7fc3c6f3b7ec0a4a5638a464788a1d548a75a73
Marik95 <alexmarchenko70@gmail.com>
2022-09-06 17:02:25 +0300

    Add HtmlReport class for making reports in html files

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -3,6 +3,7 @@ import { CsvFileReader } from "./CsvFileReader";
 import { WinsAnalysis } from "./Analyzers/WinsAnalysis";
 import { ConsoleReport } from "./reportTargets/ConsoleRecoprt";
 import { Summary } from "./Summary";
+import { HtmlReport } from "./reportTargets/HtmlReport";
 
 //Create an object that implement the DataReader interface
 const csvFileReader = new CsvFileReader('football.csv');
@@ -13,7 +14,7 @@ const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
 
 
-const summary = new Summary(new WinsAnalysis('Man United'), new ConsoleReport());
-
+// const summary = new Summary(new WinsAnalysis('Man United'), new ConsoleReport());
+const summary = new Summary(new WinsAnalysis('Man United'), new HtmlReport());
 summary.buildAndPrintReport(matchReader.matches);
 
