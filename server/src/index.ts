a239c08a2352dad0e342452b7516a4621593f0da
Marik95 <alexmarchenko70@gmail.com>
2022-09-07 19:56:28 +0300

    Cretae get and post routes for login page

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,10 @@
+import bodyParser from 'body-parser';
 import express, {Request, Response} from 'express';
 import { router } from './routes/loginRoutes';
 
 const app = express();
 
-// app.get('/', (req: Request, res: Response) => {
-//     res.send(
-//         `
-//             <div>
-//                 <h1>Hey there!</h1>
-//             </div>
-//         `
-//     )
-// });
+app.use(express.urlencoded({extended: true}));
 
 app.use(router);
 
--- server/src/routes/loginRoutes.ts
+++ server/src/routes/loginRoutes.ts
@@ -1,9 +1,33 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
+import bodyParser from "body-parser";
 
 const router = Router();
 
 router.get('/', (req, res) => {
     res.send('Hey there!');
+})
+
+router.get('/login', (req: Request, res: Response) => {
+    res.send(`
+        <form method="POST">
+        <div>
+            <label>Email</label>
+            <input name="email">
+        </div>
+        <br>
+        <div>
+            <label>Password</label>
+            <input name="password" type="password">
+        </div>
+        <button>Submit</button>
+        </form>
+    `);
+});
+
+router.post('/login', (req: Request, res: Response) => {
+    const {email, password} =  req.body;
+
+    res.send(email + " " + password);    
 })
 
 export {router};
