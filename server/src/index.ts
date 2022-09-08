818742d89f6973565e2cef9716aa99de04857546
Marik95 <alexmarchenko70@gmail.com>
2022-09-08 20:01:59 +0300

    Setup login and loout functionality in logitRoutes file

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,11 @@
-import bodyParser from 'body-parser';
 import express, {Request, Response} from 'express';
 import { router } from './routes/loginRoutes';
+import cookieSession from 'cookie-session';
 
 const app = express();
 
 app.use(express.urlencoded({extended: true}));
-
+app.use(cookieSession({keys: ['asdfasdfasdf']}));
 app.use(router);
 
 app.listen(3001, () => {
--- server/src/routes/loginRoutes.ts
+++ server/src/routes/loginRoutes.ts
@@ -7,10 +7,6 @@ interface RequestWithBody extends Request {
 
 const router = Router();
 
-router.get('/', (req, res) => {
-    res.send('Hey there!');
-})
-
 router.get('/login', (req: RequestWithBody, res: Response) => {
     res.send(`
         <form method="POST">
@@ -32,11 +28,40 @@ router.post('/login', (req: RequestWithBody, res: Response) => {
     const {email, password} =  req.body;
 
     
-    if(email){
-        res.send(email.toLowerCase());
-    } else {
-        res.send("You must provide an email")
-    }    
+    if(email && password && email === "admin@admin.com" && password === "password"){
+        //show that user is logged in
+        req.session = { loggedIn: true};
+
+        // Redirect to the root route
+        res.redirect('/');
+
+    }else {
+        res.send('Invalid email or password');
+    }
+
 })
+
+router.get('/', (req: RequestWithBody, res: Response) => {
+    // req.session
+    if(req.session && req.session.loggedIn){
+        res.send(
+            `<div>
+                <div>You are logged in!</div>
+                <a href="/logout">Logout</a>
+            </div>`
+        );
+    } else {
+        res.send(
+            `<div>
+                <div>You are not logged in!</div>
+                <a href="/login">Login</a>
+            </div>`
+        );
+    }
+})
+router.get('/logout', (req: RequestWithBody, res: Response) =>{
+    req.session = undefined;
+    res.redirect('/');
+});
 
 export {router};
