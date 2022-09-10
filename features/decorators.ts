8ccbab0789e0196d86bab7f37fbec100388be4d4
Marik95 <alexmarchenko70@gmail.com>
2022-09-10 23:54:24 +0300

    Add decorators factory, to make decoreator function more flexible

--- features/decorators.ts
+++ features/decorators.ts
@@ -5,21 +5,22 @@ class DecoratedCar {
         return `This car color id ${this.color}`;
     }
 
-    @logError    
+    @logError('Darn! The car crashed!')    
     drive(): void {
         throw new Error();
         console.log('I am driving');
     }
 }
+function logError(errorMessage: string){
+    return function logError(target: any, key: string, desc: PropertyDescriptor): void {
+        const method = desc.value;
 
-function logError(target: any, key: string, desc: PropertyDescriptor): void {
-    const method = desc.value;
-
-    desc.value = function() {
-        try{
-            method();
-        } catch (e) {
-            console.log('Darn! The car crashed!');
+        desc.value = function() {
+            try{
+                method();
+            } catch (e) {
+                console.log(errorMessage);
+            }
         }
     }
 }
