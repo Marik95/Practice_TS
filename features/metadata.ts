a9c2f7db200735bb10fc03fbedef812d1ad69fdb
Marik95 <alexmarchenko70@gmail.com>
2022-09-14 15:46:02 +0300

    One more example of metadata

--- features/metadata.ts
+++ features/metadata.ts
@@ -1,22 +1,59 @@
 import "reflect-metadata";
 
-const box = {
-    color: "black"
-};
+@printMetadata
+class Car {
+    color: string = 'black';
+
+    @markFunction('BIG SECRET2')
+    drive(): void {
+        console.log('I am driving');
+    }
+}
+
+function printMetadata(target: typeof Car){
+    for(let key in target.prototype) {
+        const info = Reflect.getMetadata('info', target.prototype, key);
+        console.log(key);
+    }
+    // console.log(target.prototype);
+}
+
+
+function markFunction(secretInfo: string){
+    return function(target: Car, key: string){
+        Reflect.defineMetadata('info', secretInfo, target, key);
+        // const info = Reflect.getMetadata('info', target, key);
+        // console.log(info);
+    }
+}
+
+// const info = Reflect.getMetadata('info', Car.prototype, 'drive');
+
+// console.log(info);
+
+
+
+
+
+
+// ============================================ Simple Examples of Metadata===========================
+// const box = {
+//     color: "black"
+// };
 
 // Metadata for object
-Reflect.defineMetadata('note', 'hello', box);
-Reflect.defineMetadata('width', 30, box);
+// Reflect.defineMetadata('note', 'hello', box);
+// Reflect.defineMetadata('width', 30, box);
 
-const note = Reflect.getMetadata('note', box);
-const width = Reflect.getMetadata('width', box);
+// const note = Reflect.getMetadata('note', box);
+// const width = Reflect.getMetadata('width', box);
 
-console.log(note, width);
+// console.log(note, width);
 
 // Metadata for object property
 
-Reflect.defineMetadata('note', 'hello', box, 'color');
+// Reflect.defineMetadata('note', 'hello', box, 'color');
 
-const noteColor = Reflect.getMetadata('note', box, 'color');
+// const noteColor = Reflect.getMetadata('note', box, 'color');
 
-console.log(noteColor);
+// console.log(noteColor);
